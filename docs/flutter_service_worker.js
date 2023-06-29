8868758c820a72b9f308770edf6f5f4f7918dace
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2023-06-28 21:35:03 -0300

    docs update

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "d3989d7cd91c4f409d222126b20787e0",
-"/": "d3989d7cd91c4f409d222126b20787e0",
-"main.dart.js": "03cbcfe66f1cc440043aaa8226c5a295",
+"index.html": "53263413ee5e520b43ee94aab92cd859",
+"/": "53263413ee5e520b43ee94aab92cd859",
+"main.dart.js": "304aa13ed0378ba5ece961e8bf3ec247",
 "manifest.json": "345a7ee0bc61f5a2fe96a6db6c8f50a8",
 "version.json": "b7c0f6392b8d0832fb0337aa2f004201"
 };
